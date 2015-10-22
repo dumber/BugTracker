9a3d701dd995ffefc2a5707a3dd3984be9f47333
dumber <dumber@users.noreply.github.com>
2015-10-22 16:25:55 +0200

    minor change, main updated a little

--- src/application/BugTrackerApp.java
+++ src/application/BugTrackerApp.java
@@ -6,112 +6,76 @@ package application;
 import helper.MySqlDataSourceSingleton;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.DatabaseMetaData;
 
-import test.FactoryTest;
 
+
+
+
+
+
+
+import controller.UserInterfaceControl;
+import test.FactoryTest;
+import view.RootLayoutController;
+import view.TicketOverviewConroller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import model.Action;
 import model.DetectionPhase;
+import model.TicketHeader;
 import model.dao.factory.ActionDaoFactory;
 import model.dao.factory.DetectionPhaseDaoFactory;
 import model.dao.factory.PriorityDaoFactory;
+import model.dao.secure.PasswordHash;
 
 /**
  * @author dumber
  *
  */
 public class BugTrackerApp extends Application {
-	private Stage primaryStage;
-	private BorderPane rootLayout;
+	private UserInterfaceControl gui;
 	
 	@Override
 	public void start(Stage primaryStage) {
-		this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("BugTracker");
-        
-        initRootLayout();
-        showTicketOverview();
+		gui = new UserInterfaceControl(primaryStage);
+        gui.initRootLayout();
+        gui.showWelcomeTab();
 	}
-	
-    /**
-     * Initializes the root layout.
-     */
-    public void initRootLayout() {
-        try {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(BugTrackerApp.class.getResource("../view/RootLayout.fxml"));
-            rootLayout = (BorderPane) loader.load();
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    /**
-     * Shows the ticket overview inside the root layout.
-     */
-    public void showTicketOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(BugTrackerApp.class.getResource("../view/TicketOverview.fxml"));
-            TabPane ticketOverview = (TabPane) loader.load();
-            Tab tab = new Tab();
-//            tab.setText("Welcome");
-            tab.setContent(ticketOverview);
-            TabPane tp = new TabPane();
-            tp.getTabs().add(tab);
-            // Set person overview into the center of root layout.
-            rootLayout.setCenter(tp);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }    
-    
-    /**
-     * Returns the main stage.
-     * @return
-     */
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
-    
 
-    
+  
 	public static void main(String[] args) {
 		MySqlDataSourceSingleton my = MySqlDataSourceSingleton.getInstance();
 		try {
-			my.setupConnection("dumber", "asdfQq22");		
-//			my.getDatabaseMetaData();
-//			my.executeQueryDemo();
-			FactoryTest test = new FactoryTest();
-			test.runSelectTest();
-//			test.runInsertTest();
-			test.printMaxTest();
-//			test.runUpdateTest();
-			test.runDeleteTest();
+			my.setupConnection("dumber", "Test1234");		
+			
 //			DatabaseMetaData meta = my.getConn().getMetaData();
 //			String[] types = {"TABLE"};
 //			ResultSet rs_t = meta.getTables(null, null, "%", types);
 //			table_colums_n_types = new ArrayList<Map<String, List<Map<String,String>>>>();
 //			while (rs_t.next()) {
 //				String table = rs_t.getString("TABLE_NAME");
-				test.runSelectAllTest("actions");
+				FactoryTest test = new FactoryTest("users");
+				test.runSelectTest(2);
+//				test.runInsertTest();
+//				test.printMaxTest();
+//				test.runUpdateTest();
+//				test.runDeleteTest();
+				test.runSelectAllTest();
 //			}
-//			DetectionPhaseDaoFactory adf = new DetectionPhaseDaoFactory();
+//			ActionDaoFactory adf = new ActionDaoFactory();
+//			System.out.println("mivan?");
+			//+adf.findActionById(1).debug());
+//			System.out.println(adf.findActionById(1).debug());
 //			DetectionPhase a = new DetectionPhase(adf.findDetectionPhaseById(1));
 //			System.out.println("Success: " );
 //			System.out.println(a.toString());
@@ -134,7 +98,7 @@ public class BugTrackerApp extends Application {
 //			for(DetectionPhase i : alist) {
 //				System.out.println(i.debug());
 //			}
-			my.MySqlDataSourceCloseConections();
+//			my.MySqlDataSourceCloseConections();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -147,7 +111,10 @@ public class BugTrackerApp extends Application {
 		} catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}/* catch (NoSuchAlgorithmException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}*/		
 //		launch(args);
 	}
 }
